1d8de405a3bacf3857853f223fa2de551044400b
James Talton <jtalton@redhat.com>
2022-01-19 13:27:33 -0500

    build fixes
    Signed-off-by: James Talton <jtalton@redhat.com>

--- src/Step.tsx
+++ src/Step.tsx
@@ -1,5 +1,6 @@
 import { Fragment, ReactNode } from 'react'
+import { HiddenFn } from './inputs/FormWizardInput'
 
-export function Step(props: { label: string; children?: ReactNode; id?: string }) {
+export function Step(props: { label: string; children?: ReactNode; id?: string; hidden?: HiddenFn }) {
     return <Fragment>{props.children}</Fragment>
 }
