efa3d7a24144e372dc07d0eac282349b3611906d
James Talton <jtalton@redhat.com>
2022-01-20 12:54:30 -0500

    fix
    Signed-off-by: James Talton <jtalton@redhat.com>

--- wizards/Ansible/AnsibleExample.tsx
+++ wizards/Ansible/AnsibleExample.tsx
@@ -1,19 +1,17 @@
-import { useCallback, useMemo } from 'react'
+import { useMemo } from 'react'
 import { useHistory } from 'react-router'
-import { RouteE } from '../Routes'
 import { AnsibleWizard } from './AnsibleWizard'
 
 export function AnsibleExample() {
     const history = useHistory()
-    const onSubmit = useCallback(
-        (data: object) => {
-            sessionStorage.setItem('results', JSON.stringify(data))
-            history.push(RouteE.Results)
-            return Promise.resolve()
-        },
-        [history]
-    )
     const credentials = useMemo(() => ['my-install-ansible-creds', 'my-upgrade-ansible-creds'], [])
     const namespaces = useMemo(() => ['default'], [])
-    return <AnsibleWizard onSubmit={onSubmit} credentials={credentials} namespaces={namespaces} />
+    return (
+        <AnsibleWizard
+            credentials={credentials}
+            namespaces={namespaces}
+            onSubmit={() => Promise.resolve()}
+            onCancel={() => history.push('.')}
+        />
+    )
 }
