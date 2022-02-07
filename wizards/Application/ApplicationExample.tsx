d893af05fcbfc9346276cb82634ab17174453729
randybrunopiverger <rbrunopi@redhat.com>
2022-02-07 11:29:39 -0500

    application wizard example
    Signed-off-by: randybrunopiverger <rbrunopi@redhat.com>

--- wizards/Application/ApplicationExample.tsx
+++ wizards/Application/ApplicationExample.tsx
@@ -8,6 +8,8 @@ export function ApplicationExample() {
     const servers = useMemo(() => ['default', 'server-1', 'server-2'], [])
     const ansibleCredentials = useMemo(() => ['credential1', 'credential2'], [])
     const placements = useMemo(() => ['placement-1', 'placement-2'], [])
+    const gitChannels = useMemo(() => ['git-channel-1', 'git-channel-2'], [])
+    const helmChannels = useMemo(() => ['helm-channel-1', 'helm-channel-2'], [])
     return (
         <ApplicationWizard
             ansibleCredentials={ansibleCredentials}
@@ -15,6 +17,8 @@ export function ApplicationExample() {
             namespaces={namespaces}
             onCancel={() => history.push('.')}
             placements={placements}
+            gitChannels={gitChannels}
+            helmChannels={helmChannels}
         />
     )
 }
