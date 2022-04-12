cf49620cc19b1e08851921fa951665dd0247448b
randybrunopiverger <rbrunopi@redhat.com>
2022-04-12 16:16:19 -0700

    Remove clusterset.metadata.namespace references.
    Signed-off-by: randybrunopiverger <rbrunopi@redhat.com>

--- wizards/Argo/ArgoWizard.tsx
+++ wizards/Argo/ArgoWizard.tsx
@@ -740,11 +740,7 @@ function ArgoWizardPlacementSection(props: {
         props.clusterSetBindings
             ?.filter((clusterSetBinding) => clusterSetBinding.metadata?.namespace === applicationSet?.metadata?.namespace)
             .filter((clusterSetBinding) =>
-                props.clusterSets?.find(
-                    (clusterSet) =>
-                        clusterSet.metadata?.name === clusterSetBinding.spec?.clusterSet &&
-                        clusterSet.metadata?.namespace === applicationSet?.metadata?.namespace
-                )
+                props.clusterSets?.find((clusterSet) => clusterSet.metadata?.name === clusterSetBinding.spec?.clusterSet)
             )
             .map((clusterSetBinding) => clusterSetBinding.spec?.clusterSet ?? '') ?? []
 
