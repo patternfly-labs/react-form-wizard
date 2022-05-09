f8c2b5ef6d880686e1a77115ffb4d0bd29598612
James Talton <jtalton@redhat.com>
2022-05-09 17:55:11 -0400

    linting
    Signed-off-by: James Talton <jtalton@redhat.com>

--- wizards/Argo/ArgoExamples.tsx
+++ wizards/Argo/ArgoExamples.tsx
@@ -71,11 +71,11 @@ export function CreateApplicationSet() {
             placements={placements}
             channels={channels}
             timeZones={timeZones}
-            getGitRevisions={async (channel) => {
+            getGitRevisions={async () => {
                 await new Promise((resolve) => setTimeout(resolve, 1000))
                 return Promise.resolve(['branch-1', 'branch-2'])
             }}
-            getGitPaths={async (channelPath, branch) => {
+            getGitPaths={async () => {
                 await new Promise((resolve) => setTimeout(resolve, 1000))
                 return Promise.resolve(['path-1', 'path-2'])
             }}
