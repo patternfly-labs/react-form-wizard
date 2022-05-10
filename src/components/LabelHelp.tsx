f913e81c81cbfb05e93c582418bd17b9e2dd5014
rhowingt@redhat.com <rhowingt@redhat.com>
2022-05-10 17:07:02 -0400

    update labelHelp component
    Signed-off-by: rhowingt@redhat.com <rhowingt@redhat.com>

--- src/components/LabelHelp.tsx
+++ src/components/LabelHelp.tsx
@@ -1,9 +1,9 @@
 import { Popover } from '@patternfly/react-core/dist/js/components//Popover'
 import { Button } from '@patternfly/react-core/dist/js/components/Button'
 import HelpIcon from '@patternfly/react-icons/dist/js/icons/help-icon'
-import { Fragment } from 'react'
+import { Fragment, ReactNode } from 'react'
 
-export function LabelHelp(props: { id: string; labelHelp?: string; labelHelpTitle?: string }): JSX.Element {
+export function LabelHelp(props: { id: string; labelHelp?: ReactNode; labelHelpTitle?: string }): JSX.Element {
     return props.labelHelp ? (
         <Popover id={`${props.id}-label-help-popover`} headerContent={props.labelHelpTitle} bodyContent={props.labelHelp}>
             <Button
